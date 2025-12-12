565a81404d6286a57c6dee0faab66f7d3b2351c0
Nearo007 <araujo8166@gmail.com>
2025-12-12 09:58:56 -0300

    changing

--- main.py
+++ main.py
@@ -1,46 +1,57 @@
 import mido
 import time
+import notes as key
+
 # mensagem = mido.Message('note_on')
 # print(mensagem.bytes())
 
-outport = mido.open_output('CASIO USB-MIDI 1')
 
-e2 = mido.Message('note_on', channel=0, note=40, velocity=64, time=0)
-g2 = mido.Message('note_on', channel=0, note=43, velocity=64, time=0)
-g_sharp2 = mido.Message('note_on', channel=0, note=44, velocity=64, time=0)
-a3 = mido.Message('note_on', channel=0, note=45, velocity=64, time=0)
-a_sharp3 = mido.Message('note_on', channel=0, note=46, velocity=64, time=0)
-b3 = mido.Message('note_on', channel=0, note=47, velocity=64, time=0)
-c3 = mido.Message('note_on', channel=0, note=48, velocity=64, time=0)
-d3 = mido.Message('note_on', channel=0, note=50, velocity=64, time=0)
-d_sharp3 = mido.Message('note_on', channel=0, note=51, velocity=64, time=0)
-f3 = mido.Message('note_on', channel=0, note=53, velocity=64, time=0)
-g3 = mido.Message('note_on', channel=0, note=55, velocity=64, time=0)
-e3 = mido.Message('note_on', channel=0, note=52, velocity=64, time=0)
+
+def get_output_names():
+    ports = mido.get_output_names()
+    print(ports)
+    return ports
+
+def get_output_port():
+    for o in get_output_names():
+        if o.find('MIDI') != -1:
+            return o
+        
+def get_virtual_output_port():
+    for o in get_output_names():
+        if o.find('virtual') != -1:
+            return o
+
+def get_input_data():
+    with mido.open_input() as port:
+        while True:
+            for message in  port:
+                print(message)
+                
+def get_interval_speed(bpm, time_signature=2):
+    bps = 60 / bpm
+    return bps / time_signature
+
+
+def play_output(outport, chord_progression, interval_speed):
+    while True:
+        for chord in chord_progression:
+            for note in chord:
+                outport.send(note)
+            
+            time.sleep(interval_speed)
+            
+            for note in chord:
+                outport.send(mido.Message('note_off', note.note))
+
 
 # chord_progression = [[a3], [c3, e3], [e2], [c3, a3]]
+chord_progression = [[key.c3], [key.d_sharp3, key.g3], [key.a_sharp3], [key.d3, key.f3], [key.g_sharp2], [key.c3, key.d_sharp3], [key.g2], [key.b3, key.d3]]
 
-chord_progression = [[c3], [d_sharp3, g3], [a_sharp3], [d3, f3], [g_sharp2], [c3, d_sharp3], [g2], [b3, d3]]
+bpm = 100
+interval_speed = get_interval_speed(bpm, 1)
 
-interval_speed = 0.5
+output_port = mido.open_output(get_output_port())
 
-while True:
-    for chord in chord_progression:
-        for note in chord:
-            outport.send(note)
-        
-        time.sleep(interval_speed)
-        
-        for note in chord:
-            outport.send(mido.Message('note_off', note.note))
-
-# def get_output_names():
-#     print(mido.get_output_names())
-
-# with mido.open_input() as port:
-#     for message in  port:
-#         print(message)
-
-# print(mido.get_input_names())
-
-#45 /// 48 52 /// 40 /// 48 52
+play_output(output_port, chord_progression, interval_speed)
+get_output_port()
---
+++ notes.py
@@ -0,0 +1,14 @@
+import mido
+
+e2 = mido.Message('note_on', channel=0, note=40, velocity=64, time=0)
+g2 = mido.Message('note_on', channel=0, note=43, velocity=64, time=0)
+g_sharp2 = mido.Message('note_on', channel=0, note=44, velocity=64, time=0)
+a3 = mido.Message('note_on', channel=0, note=45, velocity=64, time=0)
+a_sharp3 = mido.Message('note_on', channel=0, note=46, velocity=64, time=0)
+b3 = mido.Message('note_on', channel=0, note=47, velocity=64, time=0)
+c3 = mido.Message('note_on', channel=0, note=48, velocity=64, time=0)
+d3 = mido.Message('note_on', channel=0, note=50, velocity=64, time=0)
+d_sharp3 = mido.Message('note_on', channel=0, note=51, velocity=64, time=0)
+f3 = mido.Message('note_on', channel=0, note=53, velocity=64, time=0)
+g3 = mido.Message('note_on', channel=0, note=55, velocity=64, time=0)
+e3 = mido.Message('note_on', channel=0, note=52, velocity=64, time=0)
