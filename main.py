ee97d4de096d05e479b87d3de3b212c475074a78
Nearo007 <araujo8166@gmail.com>
2025-12-28 16:45:36 -0300

    added scale size flexibility (for pentatonic and more)

--- main.py
+++ main.py
@@ -9,6 +9,6 @@ interval_speed = get_interval_speed(bpm, 1)
 output_port = mido.open_output(get_output_port())
 output_port = mido.open_output(get_virtual_output_port())
 
-scale = get_playable_scale(1, 1, 2) # mode (0=major, 1=minor), key 1-12 (C to B), register 1-7 (octaves)
+scale = get_playable_scale(2, 1, 2) # mode (0=major, 1=minor), key 1-12 (C to B), register 1-7 (octaves)
 
 play_output(output_port, scale, interval_speed, 0.5)
--- scales.py
+++ scales.py
@@ -4,11 +4,13 @@ def get_note_ids(mode, key, register):
     note_ids = []
     
     if mode == 0:
-        steps = [0, 2, 2, 1, 2, 2, 2, 1]
+        steps = [0, 2, 2, 1, 2, 2, 2, 1] # major
     
-    else:
-        steps = [0, 2, 1, 2, 2, 1, 2, 2]
+    elif mode == 1:
+        steps = [0, 2, 1, 2, 2, 1, 2, 2] # minor
     
+    elif mode == 2:
+        steps = [0, 3, 2, 1, 1, 3, 2] # minor blues
     B0_MIDI = 23
     current_id = B0_MIDI + key
     for i in steps:
@@ -20,16 +22,9 @@ def get_note_ids(mode, key, register):
 def get_scale(mode=0, key=1, register=3): # mode (0=major, 1=minor), key 1-12 (C to B), register 1-7 (octaves)
     note_ids = get_note_ids(mode, key, register)
 
-    scale = [
-        mido.Message('note_on', channel=0, note=note_ids[0], velocity=64, time=0),
-        mido.Message('note_on', channel=0, note=note_ids[1], velocity=64, time=0),
-        mido.Message('note_on', channel=0, note=note_ids[2], velocity=64, time=0),
-        mido.Message('note_on', channel=0, note=note_ids[3], velocity=64, time=0),
-        mido.Message('note_on', channel=0, note=note_ids[4], velocity=64, time=0),
-        mido.Message('note_on', channel=0, note=note_ids[5], velocity=64, time=0),
-        mido.Message('note_on', channel=0, note=note_ids[6], velocity=64, time=0),
-        mido.Message('note_on', channel=0, note=note_ids[7], velocity=64, time=0)
-    ]
+    scale = []
+    for i in note_ids:
+        scale.append(mido.Message('note_on', channel=0, note=i, velocity=64, time=0))
     
     return scale
 
