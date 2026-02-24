f69576fc68c69f2471bd9e5b000bed169ff35094
Nearo007 <araujo8166@gmail.com>
2026-02-24 19:19:43 -0300

    .

--- main.py
+++ main.py
@@ -24,8 +24,9 @@
 # player.play_sequence(sequence=sequence, interval_speed=interval_speed, staccato=1)
 
 from src.infrastructure.ui.cli.cli_app import run as run_cli_app
-from src.infrastructure.ui.tkinter.tk_app import run as run_window_app
+# from src.infrastructure.ui.tkinter.tk_app import run as run_window_app #TODO fix import running
 
 if __name__ == '__main__':
-    # run_cli_app()
-    run_window_app()
+    #run_cli_app()
+    # run_window_app()
+    pass
