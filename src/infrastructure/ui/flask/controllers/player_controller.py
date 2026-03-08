e1a43580a358a963f73fd7caed44d0016b6a3ee2
Nearo007 <araujo8166@gmail.com>
2026-03-08 13:25:02 -0300

    add change port option

--- src/infrastructure/ui/flask/controllers/player_controller.py
+++ src/infrastructure/ui/flask/controllers/player_controller.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, jsonify, render_template, request, redirect
-from src.infrastructure.ui.flask.instance import player_service
+from src.infrastructure.ui.flask.instance import player_service, midi_adapter
 from src.domain.music_theory import get_scale_notes, get_chord
 
 player_service_bp = Blueprint('player_service_bp', __name__)
@@ -14,6 +14,7 @@ def piano():
 
 @player_service_bp.route("/play", methods=['POST'])
 def play():
+    player_service.set_staccato(staccato_value=0.5) #TODO hard coded
     key_num = request.form.get("key_num")
     try:
         key_num = int(key_num)
@@ -25,11 +26,18 @@ def play():
 
 @player_service_bp.route("/chord-lab", methods=['GET'])
 def chord_lab():
-    return render_template("chord-lab.html")
+    available_ports = midi_adapter.available_outports()
+    
+    default_port = midi_adapter.get_current_port()
+    default_port_id = available_ports.index(default_port)
+    available_ports.pop(default_port_id)
+    available_ports.insert(0, default_port)
+    
+    return render_template("chord-lab.html", available_ports=available_ports)
 
 @player_service_bp.route("/chord-lab/start-progression", methods=['POST'])
 def start_progression():
-    player_service.set_bpm(bpm=80) #TODO hard coded
+    player_service.set_bpm(bpm=80, time_signature=0.5) #TODO hard coded
     player_service.set_staccato(staccato_value=1) #TODO hard coded
     
     progression = request.json['progression']
@@ -47,3 +55,11 @@ def start_progression():
 def stop_progression():
     player_service.stop_loop()
     return "", 204
+
+@player_service_bp.route("/chord-lab/set-port", methods=['POST'])
+def set_port():
+    player_service.stop_loop()
+    port = request.json['port']
+    midi_adapter.set_outport(port)
+    player_service.port = midi_adapter
+    return ", 204"
